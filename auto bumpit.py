7e914da8426ce7edf4dab7169669595560b861d6
RussianAssault <125305076+RussianAssaulter@users.noreply.github.com>
2023-04-17 19:33:25 +0100

    Add files via upload

---
+++ auto bumpit.py
@@ -0,0 +1,32 @@
+import requests
+import time
+import colorama
+from colorama import *
+
+
+
+token = input("token: ")
+channelid = input("channelid: ")
+
+message = {
+    'content': '/bump'
+}
+
+header = {
+    'authorization': token
+}
+
+r = requests.post(f"https://discord.com/api/v8/channels/{channelid}/messages", data=message, headers=header)
+
+while True:
+    r
+    time.sleep(3600)
+    if r.status_code == 204:
+        print(Fore.GREEN+"Succesfully Bumped!")
+    else:
+        print("Failed To Bump!")
+
+ 
+ #simple auto-bumpit python script using plain post requests.
+
+
